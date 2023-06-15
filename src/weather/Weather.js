ab7e0b0dd476891f929c852728c966e349041666
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2023-06-15 13:09:55 +0200

    add weather js and weather css

--- src/weather/Weather.js
+++ src/weather/Weather.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
-
+import './Weather.css'
 // https://danepubliczne.imgw.pl/api/data/synop/
 
 function Weather() {
-
+    // react hook on state
     const [danePogodowe, setDanePogodowe] = useState([])
 
     useEffect(() => {
@@ -13,14 +13,16 @@ function Weather() {
         const getData = () => {
             axios
                 .get('https://danepubliczne.imgw.pl/api/data/synop/')
-                .then((dane) => {
-                    console.log(dane.data);
-                    setDanePogodowe(dane.data)
-                })
-                .catch((error) => {
-                    console.log(error);
-                    return null;
-                })
+                .then(
+                    (dane) => {
+                        console.log(dane.data);
+                        setDanePogodowe(dane.data)
+                    })
+                .catch(
+                    (error) => {
+                        console.log(error);
+                        return null;
+                    })
         }
         getData();
 
@@ -30,13 +32,26 @@ function Weather() {
         <div>
             <div>Dane pogodowe</div>
             <div>
-                {danePogodowe.map(({ id_stacji, stacja }) => {
-                    return <div key={id_stacji} >{stacja}</div>;
-                })}
+                {
+                    danePogodowe.map(
+                        ({ stacja, temperatura, cisnienie }) => {
+                            return <div className='weather_card'>
+                                <div className='weather_title'>Dane ze stacji {stacja}</div>
+                                <div className='weather_params'>
+                                    Temperatura: {temperatura} &deg;C, <br />
+                                    Ciśnienie: {cisnienie} hPa
+                                </div>
+                            </div>
+                        })
+                }
 
             </div>
         </div >
     )
 }
-
+// {/* {
+//                     danePogodowe.map(({ id_stacji, stacja }) => {
+//                         return <div key={id_stacji} >{stacja}</div>;
+//                     })
+//                 } */}
 export default Weather
